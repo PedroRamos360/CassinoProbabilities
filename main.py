9cc00f55602089a6ada6ee759132ad4401d5de05
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2024-02-06 09:44:54 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import random
 
 choices = ["V", "P"]
 BET = "P"
-balance = 1000
-bet_value = 10
+balance = 1000000
+start_balance = 1000000
+original_bet = 10
+bet_value = 1
 
 lastThreeColors = []
 results =[]
@@ -11,33 +13,37 @@ losts = 0
 wins = 0
 
 is_streak = False
-for j in range(10):
-    for i in range(100):
+for j in range(100):
+    for i in range(1000):
         choice = random.choice(choices)
-        if len(lastThreeColors) == 5 and all(color == "V" for color in lastThreeColors):
+        if len(lastThreeColors) == 4 and all(color == "V" for color in lastThreeColors):
             is_streak = True
         
         if (is_streak):
             balance -= bet_value
+            if (balance < 0):
+                balance = 0
+                break
             if choice == BET:
                 balance += bet_value * 2
+                bet_value = original_bet
                 is_streak = False
             else:
                 bet_value *= 2
                 
         lastThreeColors += choice
-        if len(lastThreeColors) > 5:
+        if len(lastThreeColors) > 4:
             lastThreeColors.pop(0)
     results.append(balance)
     if balance < 1000: losts += 1
     if balance > 1000: wins += 1
-    balance = 1000
+    balance = start_balance
 avg = 0
 for r in results:
     avg += r
 avg /= len(results)
 print(results)
-print("average outcome:", avg)
+print("average outcome:", avg - start_balance)
 print("wins: ", wins)
 print("loses: ", losts)
     
