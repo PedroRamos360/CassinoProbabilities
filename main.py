5914663717b92b5cae2f513f82fd3f7f8d391587
Pedro Ramos <53490820+PedroRamos360@users.noreply.github.com>
2024-02-06 09:30:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -9,13 +9,22 @@ lastThreeColors = []
 results =[]
 losts = 0
 wins = 0
-for j in range(1000):
-    for i in range(1000):
+
+is_streak = False
+for j in range(10):
+    for i in range(100):
         choice = random.choice(choices)
         if len(lastThreeColors) == 5 and all(color == "V" for color in lastThreeColors):
+            is_streak = True
+        
+        if (is_streak):
             balance -= bet_value
             if choice == BET:
                 balance += bet_value * 2
+                is_streak = False
+            else:
+                bet_value *= 2
+                
         lastThreeColors += choice
         if len(lastThreeColors) > 5:
             lastThreeColors.pop(0)
@@ -27,6 +36,8 @@ avg = 0
 for r in results:
     avg += r
 avg /= len(results)
+print(results)
 print("average outcome:", avg)
 print("wins: ", wins)
 print("loses: ", losts)
+    
